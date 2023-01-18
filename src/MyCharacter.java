00723a667c95d94246a9eaf271408b8cdded695d
xarqill <evatosakin@gmail.com>
2023-01-18 13:24:00 +0100

    change a main class

--- src/MyCharacter.java
+++ src/MyCharacter.java
@@ -1,22 +1,19 @@
 import java.util.Scanner;
 
 public class MyCharacter {
-    // Podstawowe atrybuty dla postaci
+    // Podstawowe atrybuty i ich domyślne wartości przydzielane ----> postaci
     String profession;
     String userName;
-    int level;
+    int level = 1;
     final int maxLevel = 300;
-    int experience;
-    int requiredExperience;
-    int gold;
+    int experience = 0;
+    int requiredExperience = 10;
+    int gold = 0;
 
-    MyCharacter() {
-        // Ustawienie podstawowych wartości dla każdego atrybutu postaci
-        level = 1;
-        experience = 0;
-        gold = 0;
+    MyCharacter(String userName, String profession) {
+
+
     }
-
     boolean isEnaughExperienceToLevelUp() {
         return experience >= requiredExperience; // Sprawdzamy czy exp >= reqExp | Jeśli tak = zwraca true | Jeśli nie = zwraca false
     }
