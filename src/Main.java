a53bc8e21fe56422bc345f963068a339b2e3b568
xarqill <evatosakin@gmail.com>
2023-01-18 11:56:46 +0100

    change a main class

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class Main {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        createUserName();
+        createProfession();
+
         MyCharacter person = new MyCharacter();
     }
 
